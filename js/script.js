eb78aef424b99afba74ea4fee4ee7cc5f0c8c233
ozrdzb <ozielrdzb@gmail.com>
2017-09-25 22:40:46 -0500

    Added new file for quotes
    Separated the quotes array from the functions in diferent files.  Linked quotes.js to the HTML file.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,12 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-var quotes = [
-  {
-    quote : 'Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.',
-    source : 'Lao Tzu',
-    citation: 'Lao Tzu Quotes. (n.d.). BrainyQuote.com. Retrieved September 18, 2017, from BrainyQuote.com Web site: https://www.brainyquote.com/quotes/quotes/l/laotzu101043.html'
-  },
+function getRandomQuote() {
 
-  {
-    quote : 'One good thing about music, when it hits you, you feel no pain.',
-    source : 'Bob Marley'
-    citation : 'Bob Marley Quotes. (n.d.). BrainyQuote.com. Retrieved September 18, 2017, from BrainyQuote.com Web site: https://www.brainyquote.com/quotes/quotes/b/bobmarley109299.html'
-  },
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('loadQuote').addEventListener("click", printQuote(), false) = quotes[randomNumber];
+};
 
-  {
-    quote : 'To live is to suffer, to survive is to find some meaning in the suffering.',
-    source : 'Friedrich Nietzsche',
-    citation : 'Friedrich Nietzsche Quotes. (n.d.). BrainyQuote.com. Retrieved September 18, 2017, from BrainyQuote.com Web site: https://www.brainyquote.com/quotes/quotes/f/friedrichn179892.html'
-  };
-];
-  function getRandomQuote() {
-
-
-  };
-
-  function printQuote(text) {
-    
-  };
+function printQuote(message) {
+  
+};
